351fff96e16f8737ebfafc1ad90982763b880d53
Erik Poort <erik@mediamonks.com>
2018-03-06 11:49:18 -0300

    Change RN App ui to have message center button

--- RNApptentiveExample/App.js
+++ RNApptentiveExample/App.js
@@ -6,33 +6,35 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  showMessageCenter = () => {
+
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text style={styles.module}>
+	        Apptentive 5.x
+        </Text>
+        <Text style={styles.module}>
+	        Example project
         </Text>
         <Text style={styles.instructions}>
-          {instructions}
+          Make sure you have added this code to your own project and you have setup the appKey and appSignature.
         </Text>
+        <Button onPress={this.showMessageCenter} title="Show message center" />
       </View>
     );
   }
@@ -48,11 +50,14 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+  },
+  module: {
+    fontSize: 15,
+    textAlign: 'center',
   },
   instructions: {
+    margin: 10,
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
   },
 });
